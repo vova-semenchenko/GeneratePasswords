c678e13113b19a5dd5dc15d3c7bd6b3c98393e66
Vova Semenchenko <semenchenko133@gmail.com>
2023-12-25 09:23:13 +0200

    Add SHA256 to encrypt password.

--- Password Generator/Password.cs
+++ Password Generator/Password.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using System.Security.Cryptography;
 using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Password_Generator
 {
     public  class Password
     {
         private StringBuilder stringBuilder;
+        private SHA256Managed manager;
 
         public Password()
         {
             stringBuilder = new StringBuilder();
+            manager = new SHA256Managed();
         }
 
         public StringBuilder GetStringBuilder { get => stringBuilder; }
@@ -17,11 +21,25 @@ namespace Password_Generator
         public void AppendChar(char _char)
         {
             stringBuilder.Append(_char);
+            GetSHA256();
         }
 
         public void AppendString(string _string)
         {
             stringBuilder.Append(_string);
+            GetSHA256();
+        }
+
+        public string GetSHA256()
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(stringBuilder.ToString());
+            byte[] hash = manager.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
         }
 
         public override bool Equals(object obj)
@@ -31,8 +49,7 @@ namespace Password_Generator
 
         public bool Equals(Password passwordGenerator)
         {
-            return passwordGenerator != null &&
-                stringBuilder.ToString() == passwordGenerator.stringBuilder.ToString();
+            return passwordGenerator != null && GetSHA256() == passwordGenerator.GetSHA256();
         }
 
         public override int GetHashCode()
